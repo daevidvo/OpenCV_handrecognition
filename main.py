7bb14895e39a909a52760db58dd998865132d5c1
daevidvo <itsdavidvo@gmail.com>
2023-06-15 00:03:33 -0700

    started working on visualization of hand through cv2

--- main.py
+++ main.py
@@ -2,16 +2,51 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-
+import numpy as np
 cam = cv2.VideoCapture(2)
 
-while True:
-    ret, frame = cam.read()
+BaseOptions = mp.tasks.BaseOptions
+HandLandMarker = mp.tasks.vision.HandLandmarker
+HandLandMarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandMarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-    cv2.imshow('frame', frame) 
+def print_result(result: HandLandMarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print(f'hand land marker result: {result}')
 
-    if cv2.waitKey(1) == (27):
-        cam.release()
-        cv2.destroyAllWindows()
-        break
+options = HandLandMarkerOptions(
+    base_options=BaseOptions(model_asset_path='./hand_landmarker.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_cb=print_result)                            
+
+with HandLandMarker.create_from_options(options) as landmarker:
+    while True:
+        ret, frame=cam.read()
+
+        cam_array=np.array(frame)
+
+        mp_image=mp.Image(image_format=mp.ImageFormat.SRGB, data=cam_array)
+
+        landmarker.detect_async(mp_image)
+
+        cv2.imshow(cv2.cvtColor(mp_image, cv2.COLOR_RGB2BGR)) 
+        
+        if cv2.waitKey(1) == (27):
+            cam.release()
+            cv2.destroyAllWindows()
+            break
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
